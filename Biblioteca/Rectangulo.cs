d09fbf43db79d9c157a414c2b40080180e5ff0bb
gianfranco <gianfrancops10@gmail.com>
2025-05-23 15:27:33 -0500

    Subiendo a repositorio

---
+++ Biblioteca/Rectangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Rectangulo
+    {
+        public void area()
+        {
+            Console.Write("Ingrese la base: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la altura: ");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nÁrea: " + (b * h));
+        }
+
+        public void perimetro()
+        {
+            Console.Write("Ingrese lado 1: ");
+            int l = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese lado 2: ");
+            int l1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPerimetro: " + (l + l + l1+ l1));
+        }
+    }
+}
---
+++ Biblioteca/Trapecio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca
+{
+    public class Trapecio
+    {
+        public void area()
+        {
+            Console.Write("Ingrese la base1: ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la base2: ");
+            int b2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese la altura: ");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nÁrea: " + ((b1 + b2) / 2)*h);
+        }
+
+        public void perimetro()
+        {
+            Console.Write("Ingrese la base1: ");
+            int b1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese la base2: ");
+            int b2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese lado 1: ");
+            int l1 = int.Parse(Console.ReadLine());
+            Console.Write("Ingrese lado 2: ");
+            int l2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPerimetro: " + (b1 + b2 +l1 +l2));
+        }
+    }
+}
